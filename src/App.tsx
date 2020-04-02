3e375d37d0b2c0b6417e92e0d9e5f48ed3e4e340
Agnieszka Cieśla <agnieszka.ciesla333@gmail.com>
2020-04-02 20:34:49 +0200

    Upgrade outdated dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import { hot } from "react-hot-loader/root";
 
 import "./App.scss";
 
-const App: React.FunctionComponent<{}> = ({}) => (
+const App: React.FunctionComponent = () => (
   <>
     <p>This is my ReactJS Typescript Webpack template.</p>
     <p>I used SCSS for style definitions.</p>
+    <p>HMR and React Hot Loader are also configured...</p>
   </>
 );
 
